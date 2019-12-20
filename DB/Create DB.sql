097ae72536f1ac680a1bbeb8079599025ee5acc2
Alexander Sworski <asworski@live.de>
2019-12-20 17:19:18 +0100

    Updated DB naming

--- DB/Create DB.sql
+++ DB/Create DB.sql
@@ -2,307 +2,332 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE SCHEMA IF NOT EXISTS `eidb` DEFAULT CHARACTER SET utf8;
-USE `eidb` ;
+-- -----------------------------------------------------
+-- Schema SwE_IV_DB
+-- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Table `eidb`.`Objects`
+-- Schema SwE_IV_DB
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`Objects` (
-  `idObjects` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(300) NOT NULL,
-  `deleted` TINYINT NOT NULL DEFAULT 0,
-  `description` VARCHAR(400) NULL,
-  PRIMARY KEY (`idObjects`),
-  UNIQUE INDEX `idObjects_UNIQUE` (`idObjects` ASC),
-  UNIQUE INDEX `Name_UNIQUE` (`Name` ASC));
+CREATE SCHEMA IF NOT EXISTS `SwE_IV_DB` DEFAULT CHARACTER SET utf8 ;
+USE `SwE_IV_DB` ;
+
+-- -----------------------------------------------------
+-- Table `SwE_IV_DB`.`object`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`object` (
+  `id_object` INT NOT NULL AUTO_INCREMENT,
+  `object_name` VARCHAR(300) NOT NULL,
+  `object_deleted` TINYINT NOT NULL DEFAULT 0,
+  `object_description` VARCHAR(400) NULL,
+  PRIMARY KEY (`id_object`),
+  UNIQUE INDEX `idObjects_UNIQUE` (`id_object` ASC) ,
+  UNIQUE INDEX `Name_UNIQUE` (`object_name` ASC) )
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`Object_to_Object`
+-- Table `SwE_IV_DB`.`object_to_object`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`Object_to_Object` (
-  `prim_idObject` INT NOT NULL,
-  `sec_idObject` INT NOT NULL,
-  `Obligated` TINYINT NOT NULL,
-  PRIMARY KEY (`prim_idObject`, `sec_idObject`),
-  INDEX `secObjectid_idx` (`sec_idObject` ASC),
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`object_to_object` (
+  `prim_id_object` INT NOT NULL,
+  `sec_id_object` INT NOT NULL,
+  `mandatory` TINYINT NOT NULL,
+  PRIMARY KEY (`prim_id_object`, `sec_id_object`),
+  INDEX `secObjectid_idx` (`sec_id_object` ASC) ,
   CONSTRAINT `primObjectid`
-    FOREIGN KEY (`prim_idObject`)
-    REFERENCES `eidb`.`Objects` (`idObjects`)
+    FOREIGN KEY (`prim_id_object`)
+    REFERENCES `SwE_IV_DB`.`object` (`id_object`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `secObjectid`
-    FOREIGN KEY (`sec_idObject`)
-    REFERENCES `eidb`.`Objects` (`idObjects`)
+    FOREIGN KEY (`sec_id_object`)
+    REFERENCES `SwE_IV_DB`.`object` (`id_object`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`Categories`
+-- Table `SwE_IV_DB`.`category`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`Categories` (
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`category` (
   `Category_name` VARCHAR(45) NOT NULL,
   `deleted` TINYINT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`Category_name`));
+  PRIMARY KEY (`Category_name`))
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`Object_to_category`
+-- Table `SwE_IV_DB`.`object_to_category`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`Object_to_category` (
-  `idObject` INT NOT NULL,
-  `idcategorie` INT NOT NULL,
-  PRIMARY KEY (`idObject`, `idcategorie`),
-  INDEX `idCategorie_object_toCathegorie_idx` (`idcategorie` ASC),
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`object_to_category` (
+  `id_object` INT NOT NULL,
+  `categorie_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_object`, `categorie_name`),
+  INDEX `IdCategory_object_to_cathegory_idx` (`categorie_name` ASC) ,
   CONSTRAINT `idObject_object_to_categorie`
-    FOREIGN KEY (`idObject`)
-    REFERENCES `eidb`.`Objects` (`idObjects`)
+    FOREIGN KEY (`id_object`)
+    REFERENCES `SwE_IV_DB`.`object` (`id_object`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `idCategorie_object_toCathegorie`
-    FOREIGN KEY (`idcategorie`)
-    REFERENCES `eidb`.`Categories` (`idCategories`)
+  CONSTRAINT `IdCategory_object_to_cathegory`
+    FOREIGN KEY (`categorie_name`)
+    REFERENCES `SwE_IV_DB`.`category` (`Category_name`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`Attribues`
+-- Table `SwE_IV_DB`.`attribute`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`Attribues` (
-  `idAttribues` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`attribute` (
+  `id_attribute` INT NOT NULL AUTO_INCREMENT,
+  `attribute_name` VARCHAR(45) NOT NULL,
   `deleted` TINYINT NOT NULL DEFAULT 0,
-  `Datatype` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idAttribues`),
-  UNIQUE INDEX `idAttribues_UNIQUE` (`idAttribues` ASC),
-  UNIQUE INDEX `Name_UNIQUE` (`Name` ASC) VISIBLE);
+  `attribute_datatype` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_attribute`),
+  UNIQUE INDEX `idAttribues_UNIQUE` (`id_attribute` ASC) ,
+  UNIQUE INDEX `Name_UNIQUE` (`attribute_name` ASC) )
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`Categorie_to_Attributes`
+-- Table `SwE_IV_DB`.`category_to_attribute`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`Categorie_to_Attributes` (
-  `idCategorie` INT NOT NULL,
-  `idAttribute` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`category_to_attribute` (
+  `category_name` VARCHAR(45) NOT NULL,
+  `id_attribute` INT NOT NULL,
   `mandatory` TINYINT NULL,
-  PRIMARY KEY (`idCategorie`, `idAttribute`),
-  INDEX `idAttribute_Categorie_to_Attributes_idx` (`idAttribute` ASC),
-  CONSTRAINT `idCategorie_Categorie_to_Attributes`
-    FOREIGN KEY (`idCategorie`)
-    REFERENCES `eidb`.`Categories` (`idCategories`)
+  PRIMARY KEY (`category_name`, `id_attribute`),
+  INDEX `idAttribute_Categorie_to_Attributes_idx` (`id_attribute` ASC) ,
+  CONSTRAINT `idAttribute_Categorie_to_Attributes`
+    FOREIGN KEY (`id_attribute`)
+    REFERENCES `SwE_IV_DB`.`attribute` (`id_attribute`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `idAttribute_Categorie_to_Attributes`
-    FOREIGN KEY (`idAttribute`)
-    REFERENCES `eidb`.`Attribues` (`idAttribues`)
+  CONSTRAINT `Categorie_name_Categorie_to_Attributes`
+    FOREIGN KEY (`category_name`)
+    REFERENCES `SwE_IV_DB`.`category` (`Category_name`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`Objectentity`
+-- Table `SwE_IV_DB`.`object_entity`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`Objectentity` (
-  `idObject` INT NOT NULL,
-  `idObjectentity` INT NOT NULL AUTO_INCREMENT,
-  `deleted` TINYINT NOT NULL DEFAULT 0,
-  INDEX `idObject_Objectentity_idx` (`idObject` ASC),
-  PRIMARY KEY (`idObjectentity`, `idObject`),
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`object_entity` (
+  `id_object` INT NOT NULL,
+  `id_object_entity` INT NOT NULL AUTO_INCREMENT,
+  `object_entity_deleted` TINYINT NOT NULL DEFAULT 0,
+  INDEX `idObject_Objectentity_idx` (`id_object` ASC) ,
+  PRIMARY KEY (`id_object_entity`, `id_object`),
   CONSTRAINT `idObject_Objectentity`
-    FOREIGN KEY (`idObject`)
-    REFERENCES `eidb`.`Objects` (`idObjects`)
+    FOREIGN KEY (`id_object`)
+    REFERENCES `SwE_IV_DB`.`object` (`id_object`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`Usergroup`
+-- Table `SwE_IV_DB`.`usergroup`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`Usergroup` (
-  `idUsergroup` INT NOT NULL AUTO_INCREMENT,
-  `Usergroupname` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idUsergroup`));
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`usergroup` (
+  `id_usergroup` INT NOT NULL AUTO_INCREMENT,
+  `usergroup_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_usergroup`))
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`users`
+-- Table `SwE_IV_DB`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`users` (
-  `idusers` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
-  `userpassword` VARCHAR(45) NOT NULL,
-  `idusergroup` INT NULL,
-  PRIMARY KEY (`idusers`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
-  UNIQUE INDEX `idusers_UNIQUE` (`idusers` ASC),
-  INDEX `idusergroup_users_idx` (`idusergroup` ASC),
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`user` (
+  `id_user` INT NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(45) NOT NULL,
+  `user_password` VARCHAR(45) NOT NULL,
+  `id_usergroup` INT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE INDEX `username_UNIQUE` (`user_name` ASC) ,
+  UNIQUE INDEX `idusers_UNIQUE` (`id_user` ASC) ,
+  INDEX `idusergroup_users_idx` (`id_usergroup` ASC) ,
   CONSTRAINT `idusergroup_users`
-    FOREIGN KEY (`idusergroup`)
-    REFERENCES `eidb`.`Usergroup` (`idUsergroup`)
+    FOREIGN KEY (`id_usergroup`)
+    REFERENCES `SwE_IV_DB`.`usergroup` (`id_usergroup`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`permissions`
+-- Table `SwE_IV_DB`.`permission`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`permissions` (
-  `idpermissions` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`permission` (
+  `id_permissions` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `description` VARCHAR(400) NOT NULL,
-  PRIMARY KEY (`idpermissions`));
+  PRIMARY KEY (`id_permissions`))
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`usergroup_to_permissions`
+-- Table `SwE_IV_DB`.`usergroup_to_permission`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`usergroup_to_permissions` (
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`usergroup_to_permission` (
   `idusergroup` INT NOT NULL,
   `idpermission` INT NOT NULL,
   PRIMARY KEY (`idusergroup`, `idpermission`),
-  INDEX `idpermission_usergroup_to_permissions_idx` (`idpermission` ASC),
-  CONSTRAINT `idusergroup_usergroup_to_permissions`
+  INDEX `idpermission_usergroup_to_permissions_idx` (`idpermission` ASC) ,
+  CONSTRAINT `idusergroup_usergroup_to_permission`
     FOREIGN KEY (`idusergroup`)
-    REFERENCES `eidb`.`Usergroup` (`idUsergroup`)
+    REFERENCES `SwE_IV_DB`.`usergroup` (`id_usergroup`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `idpermission_usergroup_to_permissions`
+  CONSTRAINT `idpermission_usergroup_to_permission`
     FOREIGN KEY (`idpermission`)
-    REFERENCES `eidb`.`permissions` (`idpermissions`)
+    REFERENCES `SwE_IV_DB`.`permission` (`id_permissions`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`Comments`
+-- Table `SwE_IV_DB`.`comment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`Comments` (
-  `idComments` INT NOT NULL AUTO_INCREMENT,
-  `idobjectentity` INT NOT NULL,
-  `comment` VARCHAR(400) NOT NULL,
-  `date` TIMESTAMP NOT NULL,
-  PRIMARY KEY (`idComments`),
-  INDEX `idobjectentity_Comments_idx` (`idobjectentity` ASC),
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`comment` (
+  `id_comment` INT NOT NULL AUTO_INCREMENT,
+  `id_object_entity` INT NOT NULL,
+  `comment_text` VARCHAR(400) NOT NULL,
+  `comment_date` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id_comment`),
+  INDEX `idobjectentity_Comments_idx` (`id_object_entity` ASC) ,
   CONSTRAINT `idobjectentity_Comments`
-    FOREIGN KEY (`idobjectentity`)
-    REFERENCES `eidb`.`Objectentity` (`idObject`)
+    FOREIGN KEY (`id_object_entity`)
+    REFERENCES `SwE_IV_DB`.`object_entity` (`id_object`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`organisations`
+-- Table `SwE_IV_DB`.`organisation`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`organisations` (
-  `idorganisations` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`organisation` (
+  `id_organisation` INT NOT NULL AUTO_INCREMENT,
+  `organisation_name` VARCHAR(45) NOT NULL,
   `kostenstelle` VARCHAR(45) NULL,
-  PRIMARY KEY (`idorganisations`));
+  PRIMARY KEY (`id_organisation`))
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`customer`
+-- Table `SwE_IV_DB`.`customer`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`customer` (
-  `idcustomer` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(60) NOT NULL,
-  `tel` VARCHAR(15) NULL,
-  `fristname` VARCHAR(45) NULL,
-  `lastname` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`customer` (
+  `id_customer` INT NOT NULL AUTO_INCREMENT,
+  `customer_email` VARCHAR(60) NOT NULL,
+  `customer_tel` VARCHAR(15) NULL,
+  `customer_fristname` VARCHAR(45) NULL,
+  `lcustomer_astname` VARCHAR(45) NULL,
   `idorganisation` INT NULL,
-  PRIMARY KEY (`idcustomer`),
-  INDEX `idorganisation_customer_idx` (`idorganisation` ASC),
+  PRIMARY KEY (`id_customer`),
+  INDEX `idorganisation_customer_idx` (`idorganisation` ASC) ,
   CONSTRAINT `idorganisation_customer`
     FOREIGN KEY (`idorganisation`)
-    REFERENCES `eidb`.`organisations` (`idorganisations`)
+    REFERENCES `SwE_IV_DB`.`organisation` (`id_organisation`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`reservations`
+-- Table `SwE_IV_DB`.`reservation`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`reservations` (
-  `idobjectentity` INT NOT NULL,
-  `startdate` DATE NOT NULL,
-  `enddate` DATE NOT NULL,
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`reservation` (
+  `id_object_entity` INT NOT NULL,
+  `reservation_startdate` DATE NOT NULL,
+  `reservation_enddate` DATE NOT NULL,
   `idcustomer` INT NOT NULL,
-  `comment` VARCHAR(400) NULL,
-  `active` TINYINT NOT NULL,
-  PRIMARY KEY (`idobjectentity`, `startdate`),
-  INDEX `idcustomer_reservations_idx` (`idcustomer` ASC),
+  `reservation_comment` VARCHAR(400) NULL,
+  `reservation_active` TINYINT NOT NULL,
+  PRIMARY KEY (`id_object_entity`, `reservation_startdate`),
+  INDEX `idcustomer_reservations_idx` (`idcustomer` ASC) ,
   CONSTRAINT `idcustomer_reservations`
     FOREIGN KEY (`idcustomer`)
-    REFERENCES `eidb`.`customer` (`idcustomer`)
+    REFERENCES `SwE_IV_DB`.`customer` (`id_customer`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `idobjectentitiy_reservations`
-    FOREIGN KEY (`idobjectentity`)
-    REFERENCES `eidb`.`Objectentity` (`idObjectentity`)
+    FOREIGN KEY (`id_object_entity`)
+    REFERENCES `SwE_IV_DB`.`object_entity` (`id_object_entity`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`loans`
+-- Table `SwE_IV_DB`.`loans`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`loans` (
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`loans` (
   `idcustomer` INT NOT NULL,
   `idobjectentity` INT NOT NULL,
   `startdate` DATE NOT NULL,
   `enddate` DATE NOT NULL,
   `returndate` DATE NULL,
   PRIMARY KEY (`idobjectentity`, `startdate`),
-  INDEX `idonjectetity_loans_idx` (`idobjectentity` ASC),
+  INDEX `idonjectetity_loans_idx` (`idobjectentity` ASC) ,
   CONSTRAINT `idcustomer_loans`
     FOREIGN KEY (`idcustomer`)
-    REFERENCES `eidb`.`customer` (`idcustomer`)
+    REFERENCES `SwE_IV_DB`.`customer` (`id_customer`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `idonjectetity_loans`
     FOREIGN KEY (`idobjectentity`)
-    REFERENCES `eidb`.`Objectentity` (`idObjectentity`)
+    REFERENCES `SwE_IV_DB`.`object_entity` (`id_object_entity`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`Objectenitiy_to_Objectentity`
+-- Table `SwE_IV_DB`.`object_enitiy_to_object_entity`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`Objectenitiy_to_Objectentity` (
-  `prim_idObjectentity` INT NOT NULL,
-  `sec_idObjectentity` INT NOT NULL,
-  `Obligated` TINYINT NOT NULL DEFAULT 1,
-  PRIMARY KEY (`prim_idObjectentity`, `sec_idObjectentity`),
-  INDEX `secObjectid_idx` (`sec_idObjectentity` ASC),
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`object_enitiy_to_object_entity` (
+  `prim_id_object_entity` INT NOT NULL,
+  `sec_id_object_entity` INT NOT NULL,
+  `mandatory` TINYINT NOT NULL DEFAULT 1,
+  PRIMARY KEY (`prim_id_object_entity`, `sec_id_object_entity`),
+  INDEX `secObjectid_idx` (`sec_id_object_entity` ASC) ,
   CONSTRAINT `primObjectid_Objectenitiy_to_Objectentity`
-    FOREIGN KEY (`prim_idObjectentity`)
-    REFERENCES `eidb`.`Objectentity` (`idObjectentity`)
+    FOREIGN KEY (`prim_id_object_entity`)
+    REFERENCES `SwE_IV_DB`.`object_entity` (`id_object_entity`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `secObjectid_Objectenitiy_to_Objectentity`
-    FOREIGN KEY (`sec_idObjectentity`)
-    REFERENCES `eidb`.`Objectentity` (`idObjectentity`)
+    FOREIGN KEY (`sec_id_object_entity`)
+    REFERENCES `SwE_IV_DB`.`object_entity` (`id_object_entity`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 -- -----------------------------------------------------
--- Table `eidb`.`detetion_reason`
+-- Table `SwE_IV_DB`.`deletion`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eidb`.`detetion_reason` (
-  `objectentityid` INT NOT NULL,
-  `reason` VARCHAR(400) NULL,
-  `date` DATETIME NOT NULL,
-  PRIMARY KEY (`objectentityid`, `date`),
+CREATE TABLE IF NOT EXISTS `SwE_IV_DB`.`deletion` (
+  `id_object_entity` INT NOT NULL,
+  `deletion_reason` VARCHAR(400) NULL,
+  `deletion_date` DATETIME NOT NULL,
+  PRIMARY KEY (`id_object_entity`, `deletion_date`),
   CONSTRAINT `objektentityid_detetion_reason`
-    FOREIGN KEY (`objectentityid`)
-    REFERENCES `eidb`.`Objectentity` (`idObjectentity`)
+    FOREIGN KEY (`id_object_entity`)
+    REFERENCES `SwE_IV_DB`.`object_entity` (`id_object_entity`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
