8983ef4b8f756f032b6b620932450515ffef83cc
SimonGByte <simon.goehring00@web.de>
2019-12-29 19:28:35 +0100

    Fertiger Service aber ohne Tests, kleine Anpassungen

--- controller.py
+++ controller.py
@@ -45,6 +45,6 @@ def call_uc_create_object():
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
-    # app.debug = True
-    # app.run(host="0.0.0.0", port=5000)
+    # app.run(port=5000)
+    app.debug = True
+    app.run(host="0.0.0.0", port=5000)
