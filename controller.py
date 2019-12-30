0a90ed39acb70795a519ef144cb75fb56322e1be
SimonGByte <simon.goehring00@web.de>
2019-12-30 12:41:06 +0100

    Exceptionhandling angepasst im Controller

--- controller.py
+++ controller.py
@@ -16,8 +16,7 @@ def call_uc_create_category():
         uc_create_category.create_category(content)
         return content
     except Exception as error:
-        print(traceback.format_exc())
-        return str(error)
+        return create_error_json(str(error))
 
 
 @app.route("/api/v1.0/listCategories", methods=["GET"])
@@ -27,24 +26,34 @@ def call_uc_list_categories():
         result = uc_list_categories.list_categories()
         return result
     except Exception as error:
-        return str(error)
+        return create_error_json(str(error))
 
 
-@app.route("/api/v1.0/listAttributesForCategory", methods=["PUT"]) # put mit jeweiligem Kategoriename
+@app.route("/api/v1.0/listAttributesForCategory", methods=["PUT"])  # put mit jeweiligem Kategoriename
 def call_uc_list_attributes():
     content = request.get_json(force=True)
-    result = uc_list_attributes.get_attributes(content)
-    return result
+    try:
+        result = uc_list_attributes.get_attributes(content)
+        return result
+    except Exception as error:
+        return create_error_json(str(error))
 
 
 # dummy-Service
 @app.route("/api/v1.0/createObject", methods=["PUT"])
 def call_uc_create_object():
-    content = request.get_json(force=True)
-    return content
+    try:
+        content = request.get_json(force=True)
+        return content
+    except Exception as error:
+        return create_error_json(str(error))
 
 
 if __name__ == '__main__':
     # app.run(port=5000)
     app.debug = True
     app.run(host="0.0.0.0", port=5000)
+
+
+def create_error_json(content):
+    return {"Fehler": content}
